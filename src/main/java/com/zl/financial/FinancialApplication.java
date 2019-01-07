e161f1f0c8f60b1e049b61b814a844b148a98c7a
lgzkd <wodeshijie1234@126.com>
2019-01-07 19:59:22 +0800

    1

--- src/main/java/com/zl/financial/FinancialApplication.java
+++ src/main/java/com/zl/financial/FinancialApplication.java
@@ -2,13 +2,23 @@ package com.zl.financial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FinancialApplication {
+public class FinancialApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FinancialApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+// 注意这里要指向原先用main方法执行的Application启动类
+
+        return builder.sources(FinancialApplication.class);
+
+    }
 }
 
